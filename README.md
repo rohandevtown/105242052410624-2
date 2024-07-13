06ddd2d69f05a332c6f92455ced71fb338236a48
rohandevtown <rohankinnal@devtown.in>
2024-07-13 20:29:02 +0530

    d1nd2

---
+++ README.md
@@ -0,0 +1,19 @@
+# DOM Operations:
+
+    >> DOM: Document Object Model
+    >> Its a representation of a web tree
+
+HTML Boiler Plate:
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<h1></h1>
+</body>
+</html>
+
+DOM Representation:
+html
+head body
+title h1
---
+++ index.js
@@ -0,0 +1,45 @@
+// console.log("Hello world!")
+
+// var state = {
+//     taskList: [
+//         {
+//             imageUrl: "",
+//             taskTitle: "",
+//             tags: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageUrl: "",
+//             taskTitle: "",
+//             tags: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageUrl: "",
+//             taskTitle: "",
+//             tags: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageUrl: "",
+//             taskTitle: "",
+//             tags: "",
+//             taskDescription: ""
+//         }
+//     ]
+// }
+
+
+var state = {
+    taskList: []
+};
+
+
+// DOM Operations
+
+var taskContents = document.querySelector(".task__contents");
+var taskModal = document.querySelector(".task__modal__body")
+
+console.log(taskContents)
+console.log(taskModal)
+
