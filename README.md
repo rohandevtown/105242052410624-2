40ab9d2e56ab2e049ef963774d116e0b2d4e4ccf
rohandevtown <rohankinnal@devtown.in>
2024-07-19 19:07:30 +0530

    d4

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
     >> Its a representation of a web tree
 
 HTML Boiler Plate:
+
 <html>
 <head>
 <title></title>
@@ -17,3 +18,20 @@ DOM Representation:
 html
 head body
 title h1
+
+const data = {
+name: "rohan",
+age: 27
+}
+undefined
+console.log(data)
+VM240:1 {name: 'rohan', age: 27}
+undefined
+console.log({data})
+VM323:1 {data: {…}}data: {name: 'rohan', age: 27}[[Prototype]]: Object
+undefined
+console.log({...data})
+VM423:1 {name: 'rohan', age: 27}
+undefined
+console.log({...data, company: "DevTown"})
+VM662:1 {name: 'rohan', age: 27, company: 'DevTown'}
--- index.js
+++ index.js
@@ -57,7 +57,8 @@ const htmlTaskContent = ({id, title, description, type, url}) => `
             </div>
             <div class="card-body">
                 ${
-                    url && `<img src=${url} alt="card image top" class="card-image-top md-3 rounded-lg" />`
+                    url ? `<img width="100%" src=${url} alt="card image top" class="card-image-top md-3 rounded-lg" />`
+                    : `<img width="100%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScujirQqIFjN5GuM1565_-DIX6OyU_96HzNBl_BAX8GL0JzMs8&s" alt="card image top" class="card-image-top md-3 rounded-lg" />`
                 }
                 <h4 class="card-title">${title}</h4>
                 <p class="description trim-3-lines text-muted card-text">${description}</p>
@@ -66,7 +67,7 @@ const htmlTaskContent = ({id, title, description, type, url}) => `
                 </div>
             </div>
             <div class="card-footer">
-                <button type="button" class="btn btn-outline-primary float-right"  data-bs-toggle="modal" data-bs-target="#showTask">
+                <button type="button" class="btn btn-outline-primary float-right"  data-bs-toggle="modal" data-bs-target="#showTask" id=${id} onclick='openTask.apply(this, arguments)'>
                 Open Task</button>
             </div>
         </div>
@@ -79,7 +80,8 @@ const htmlModalContent = ({id, title, description, url}) => {
     return `
     <div id=${id}>
         ${
-                    url && `<img src=${url} alt="card image cap" class="image-fluid mb-3" />`
+                    url ? `<img width="100%" src=${url} alt="card image cap" class="image-fluid mb-3" />`
+                    : `<img width="100%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScujirQqIFjN5GuM1565_-DIX6OyU_96HzNBl_BAX8GL0JzMs8&s" alt="card image cap" class="image-fluid mb-3" />`
         }
         <strong class="text-sm text-muted">Created On ${date.toDateString()}</strong>
         <h2 class="my-3">${title}</h2>
@@ -89,4 +91,44 @@ const htmlModalContent = ({id, title, description, url}) => {
 }
 
 
-const updateLocalStorage = () => {}
+const updateLocalStorage = () => {
+    localStorage.setItem('task', JSON.stringify({
+        tasks: state.taskList,
+    }))
+}
+
+const loadInitialData = () => {
+    const localStorageCopy = JSON.parse(localStorage.task);
+
+    if(localStorageCopy) state.taskList = localStorageCopy.tasks;
+
+    state.taskList.map((cardDate)=>{
+        taskContents.insertAdjacentHTML("beforeend", htmlTaskContent(cardDate))
+    })
+}
+
+const handleSubmit = (event) => {
+    const id = `${Date.now()}`
+    const input = {
+        url: document.getElementById('imageUrl').value,
+        title: document.getElementById('taskTitle').value,
+        description: document.getElementById('taskDescription').value,
+        type: document.getElementById('tags').value,
+    };
+
+    if(input.title === "" || input.description === "" || input.type === ""){
+        return alert("Please fill all the mandatory fields!")
+    }
+
+    taskContents.insertAdjacentHTML("beforeend", htmlTaskContent({...input, id }));
+
+    state.taskList.push({...input,id});
+    updateLocalStorage();
+}
+
+const openTask = (e) => {
+    if(!e) e = window.event;
+
+    const getTask = state.taskList.find(({id})=> id === e.target.id);
+    taskModal.innerHTML = htmlModalContent(getTask);
+}
